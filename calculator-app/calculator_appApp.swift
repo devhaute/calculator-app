59860a29ef2a740bc5f828cf182bccfda9be3c18
devhaute <devhaute@gmail.com>
2021-10-15 23:07:41 +0900

    초기 환경 세팅

---
+++ calculator-app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator-app
+//
+//  Created by devhaute on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
